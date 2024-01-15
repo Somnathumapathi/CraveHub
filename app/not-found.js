fa48ec5f496355ed9512d9b94518edbf55a62301
Somnathumapathi <somnathumapathi7@gmail.com>
2024-01-15 21:30:44 +0530

    Login page

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return <center>
+        <div class="h-screen flex items-center justify-center">
+        <h1>Are you lost baby girl 😏</h1>
+        <img src="https://w0.peakpx.com/wallpaper/778/9/HD-wallpaper-404-error-404-error-glitch-glitch.jpg" class="object-contain h-100 w-96"></img>
+        </div>
+    </center>
+}
---
+++ app/page.js
@@ -0,0 +1,59 @@
+
+
+const Login = () => {
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center bg-white flex-col">
+      <h1 className="text-5xl font-bold text-green-500 mb-9 display-block">CRAVE HUB</h1>
+      
+      
+
+      
+      
+      <div className="absolute inset-1 bg-gray-800 rounded-lg z-10 p-5 relative w-[380px] h-[380px]">
+      
+        <form>
+          <h2 className="text-2xl font-semibold text-green-500 text-center mb-6">Login</h2>
+          <div className="relative flex flex-col mb-6">
+            <input
+              type="email"
+              id="email"
+              autoFocus
+              
+              className="relative z-10 border-0 border-b-2 border-green-500 h-10 bg-transparent text-gray-100 outline-none px-2 peer"
+             
+            />
+            <i className="bg-green-500 rounded w-full bottom-0 left-0 absolute h-1 -z-10 transition-transform duration-300 origin-bottom transform peer-focus:h-1 peer-placeholder-shown:h-[0.5px]"></i>
+            <label className="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-8 scale-75 top-3 left-0 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-500 text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-gray-500 peer-focus:scale-75 peer-focus:-translate-y-8">Enter Email</label>
+          </div>
+
+          <div className="relative flex flex-col mb-6">
+            <input
+              type="password"
+              id="password"
+             
+              className="relative z-10 border-0 border-b-2 border-green-500 h-10 bg-transparent text-gray-100 outline-none px-2 peer"
+              
+            />
+            <i className="bg-green-500 rounded w-full bottom-0 left-0 absolute h-1 -z-10 transition-transform duration-300 origin-bottom transform peer-focus:h-1 peer-placeholder-shown:h-[0.5px]"></i>
+            <label className="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-8 scale-75 top-3 left-0 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-500 text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-gray-500 peer-focus:scale-75 peer-focus:-translate-y-8">Enter Password</label>
+          </div>
+
+          <button
+            type="submit"
+            className="py-3 text-gray-100 bg-green-700 w-full rounded hover:bg-green-500 hover:scale-105 duration-300"
+          >
+            Login
+          </button>
+        </form>
+
+        <p className="mt-4 text-gray-600">
+          Don't have an account?{' '}     
+              <a href='/register' className="text-green-500">Register</a>
+        </p>
+      </div>
+    
+    </div>
+  );
+};
+
+export default Login;
