5b8b700543345621728dff5f43d948fbe33500fa
Somnathumapathi <somnathumapathi7@gmail.com>
2024-01-18 01:24:23 +0530

    Can add items from merchant side

---
+++ models/item.js
@@ -0,0 +1,13 @@
+class Item {
+    constructor(name, category, price, calorie, carb, protein, fat) {
+      this.name = name;
+      this.category = category;
+      this.price = price;
+      this.calorie = calorie;
+      this.carb = carb;
+      this.protein = protein;
+      this.fat = fat
+    }
+  }
+  
+  export default Item;
