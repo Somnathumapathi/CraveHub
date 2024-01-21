c86220a1795c0a1c618e5277326763d59c915fb4
shreya <shreyamrao@gmail.com>
2024-01-22 03:30:56 +0530

    order page frontend

--- components/appbar.js
+++ components/appbar.js
@@ -27,7 +27,7 @@ const AppBar = () => {
     return (
         
     <nav>
-        <div className="bg-gray-800 p-6" >
+        <div className="bg-gray-800 p-6 border-b border-white" >
             <a className="text-3xl py-4 pr-4 font-bold text-green-700 align-middle" href="homePage"><span className='text-green-600'>Crave</span>Hub</a>
             <a className="text-white p-4 align-middle" href="homePage">Home</a>
             <a className="text-white p-4 align-middle" href="orderPage">Order</a>
