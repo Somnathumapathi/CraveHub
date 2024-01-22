3164925fa1e91b0c46182311b7f3e8b2157231d2
Shreeshail k <kohallishreeshail@gmail.com>
2024-01-22 22:19:29 +0530

    changes by shreeshail

--- app/myOrdersPage/page.js
+++ app/myOrdersPage/page.js
@@ -8,7 +8,7 @@ import { onAuthStateChanged } from 'firebase/auth';
 
 const MyOrdersPage = () => {
     const [currentUser, setCurrentUser] = useState(null);
-
+    const [expandedOrderId, setExpandedOrderId] = useState(null);
      useEffect(() => {
         const unsub = onAuthStateChanged(auth, (user) => {
             setCurrentUser(user);
@@ -40,20 +40,49 @@ const MyOrdersPage = () => {
             fetchMyOrders();
         }
     }, [currentUser]);
-
+    const toggleDetails = (orderId) => {
+      setExpandedOrderId((prevExpandedOrderId) =>
+        prevExpandedOrderId === orderId ? null : orderId
+      );
+    };
+  
     return (
-        <div>
-            <AppBar pageName="My Orders" />
-            <div>{myOrders.map((order) => (
-                <div key={order.id} className="pb-5">
-                    <p className="">{order.custName}-{(order.cusId)}</p>
-                    <p>Price-{order.price}  Calories-{order.calories}</p>
-                    <p>Salad items-{order.items}</p>
-                    
+      <div classname="pl-4 ml-6 mr-6">
+      <div className="pl-6 bg-green-900 text-white">
+        <AppBar pageName="My Orders" />
+        <div className="container mx-auto mt-8 pb-5">
+          {myOrders.map((order) => (
+            <div
+              key={order.id}
+              className="border p-4 mb-4 rounded-md shadow-md bg-green-700 relative whitespace-pre-wrap">
+              
+            
+              <div className="flex justify-between items-center pb-2 mb-2">
+                <p className="text-xl font-semibold">
+                  {order.custName} - ORDER ID: {order.id}
+                </p>
+                <button
+                  className="text-white-500 cursor-pointer"
+                  onClick={() => toggleDetails(order.id)}
+                >
+                  {expandedOrderId === order.id ? '∧ Hide Details' : '∨ Show Details'}
+                </button>
+              </div>
+              {expandedOrderId === order.id && (
+                <div>
+                  <p className="text-lg">TOTAL PRICE: {order.price}</p>
+                  <p classname="text-lg">PHONE: {order.phno}</p>
+                  <p className="text-lg">ITEMS: {Array.isArray(order.items) ? order.items.join(', ') : order.items} </p>
+                  <p className="text-lg">CALORIES: {order.calories}</p>
+                  <p classname="text-lg">ADDRESS: {order.address}</p>
                 </div>
-            ))}</div>
+              )}
+            </div>
+          ))}
         </div>
-    )
-    
-}
-export default MyOrdersPage
+      </div>
+      </div>
+    );
+  };
+  
+  export default MyOrdersPage;
